8b16cfd23eba9ceaaa853c53a3c9ae4badd1062f
skeskar1 <sachin_keskar@intuit.com>
2025-01-22 20:34:26 +0530

    Added Non Fixed Functions

---
+++ NFCodeScanTest/NFCodeScanTest.cpp
@@ -0,0 +1,29 @@
+// NFCodeScanTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Test code for Code scanning
+// Author: Sachin Keskar
+// Jan 2025
+
+#include <iostream>
+#include <tchar.h>
+#include "nfcommonfunctions.h"
+
+int main()
+{
+    printf("Hello World!");
+
+    // New and delete test
+    newanddelete();
+
+    // arr mimatch test
+    int myarr[2];
+    arrmismatch(myarr);
+
+    // overflow check
+    overflowchecktest(30000, 40000);
+
+    // allocation test
+    testallocation(10, 30);
+
+}
+
+
---
+++ NFCodeScanTest/nfcommonfunctions.h
@@ -0,0 +1,9 @@
+#pragma once
+// Author: Sachin Keskar
+// Function Declarations
+
+
+void newanddelete();
+void arrmismatch(int a[7]);
+bool overflowchecktest(unsigned short x, unsigned short y);
+void testallocation(int allocations, int size); 
